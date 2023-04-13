6e8000bdc28aa77fba4322198557d97c13a725d7
Vsplesk21 <114642420+Vsplesk21@users.noreply.github.com>
2023-04-13 20:33:25 +0300

    Add files via upload

--- Конвертер.py
+++ Конвертер.py
@@ -1,5 +1,19 @@
 from PyQt5 import Qt
 from PyQt5.QtWidgets import *
+import requests
+import pandas as pd
+
+url = "https://fapi.binance.com/fapi/v1/premiumIndex"
+param = {'symbol' : 'ETHUSDT'}
+
+r = requests.get(url, params=param)
+
+if r.status_code == 200:
+    print(r.json())
+    data = r.json()
+    print(data)
+else:
+    print("Помилка")
 
 app = QApplication([])
 window = QWidget()
@@ -14,7 +28,7 @@ label2 = QLabel("Введи валюту")
 label3 = QLabel("Результат")
 
 choose_currency = QLineEdit()
-writecurrency= QLineEdit("")
+writecurrency= QLineEdit()
 outcurrency = QLineEdit()
 
 
